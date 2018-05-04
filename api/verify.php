cc811a7215218fd5ede2a6d461cef9e70d629b41
Tudor <tudordascalu1@gmail.com>
2018-05-04 13:53:55 +0200

    Verify account - update key in db

--- api/verify.php
+++ api/verify.php
@@ -1,5 +1,8 @@
 <?php
+    include('./controllers/database.php');
     $sKey = $_GET['key'];
+    // verify user in db
+    dbVerifyUser($db, $sKey);
 
     $saUsers = file_get_contents('./storage/users.txt');
     $aUsers = json_decode($saUsers);
@@ -15,3 +18,25 @@
     }
 
     echo "{'status':'error', 'message':'invalid activation token'}";
+
+    function dbUpdateAccountVerification($id, $db) {
+        try {
+            $stmt = $db->prepare('UPDATE account_verification SET verified = true, activation_key = NULL WHERE user_id = :id');
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+        } catch (PDOException $ex) {
+            echo 'exception';
+        }
+    }
+
+    function dbVerifyUser($db, $sKey) {
+        try {
+            $stmt = $db->prepare('SELECT user_id FROM account_verification WHERE activation_key = :sKey');
+            $stmt->bindValue(':sKey', $sKey);
+            $stmt->execute();
+            $jData = $stmt->fetch();
+            dbUpdateAccountVerification($jData['user_id'], $db);
+        } catch (PDOException $ex) {
+            echo 'exception';
+        }
+    }
