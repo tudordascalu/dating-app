c7c5965f7ab4b993cc694c6e64569b122b2a554c
Tudor <tudordascalu1@gmail.com>
2018-05-18 11:25:19 +0200

    Fixes account verification

--- api/verify.php
+++ api/verify.php
@@ -9,16 +9,29 @@
     exit;
     // methods
     function dbVerifyUser($sKey, $db) {
+        $access_token = uniqid();
         try {
-            $stmt = $db->prepare('UPDATE users SET verified = true, activation_key = NULL, access_token = :accessToken WHERE activation_key = :sKey');
-            $stmt->bindValue(':sKey', $sKey);
-            $stmt->bindValue(':accessToken', uniqid());
-            $stmt->execute();
-            if($stmt->rowCount() > 0) {
-                echo 'Your account is now verified!';
-                exit;
+            $stmt = $db->prepare('SELECT user_id FROM account_activation WHERE activation_key = :activationKey');
+            $stmt->bindValue(':activationKey', $sKey);
+            if($stmt->execute()) {
+                $row = $stmt->fetchObject();
+                $user_id = $row->user_id;
+                $stmt = $db->prepare('UPDATE users SET verified = true, activation_key = NULL, access_token = :accessToken WHERE id = :id');
+                $stmt->bindValue(':accessToken', $access_token);
+                $stmt->bindValue(':id', $user_id);
+                $stmt->execute();
+                if($stmt->rowCount() > 0) {
+                    $stmt = $db->prepare('INSERT INTO account_verification VALUES(:id, :accessToken)');
+                    $stmt->bindValue(':accessToken', $access_token);
+                    $stmt->bindValue(':id', $user_id);
+                    $stmt->execute();
+                    
+                    // success
+                    echo 'Your account is now verified!';
+                    exit;
+                }
             }
         } catch (PDOException $ex) {
-            echo 'Server error';
+            echo $ex;
         }
     }
