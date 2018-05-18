ba1c6efb36e9c0a8353875565c6c2febee1e12b2
Tudor <tudordascalu1@gmail.com>
2018-05-18 12:18:13 +0200

    Save location for user

--- api/helpers/locationHelper.php
+++ api/helpers/locationHelper.php
@@ -1,12 +1,18 @@
 <?php
-    function dbSaveLocation($sId, $sLat, $sLong, $db) {
+    function dbSaveLocation($accessToken, $sLat, $sLong, $db) {
         try {
-            $stmt = $db->prepare('UPDATE users SET latitude = :latitude, longitude = :longitude WHERE access_token = :accessKey');
-            $stmt->bindValue(':latitude', $sLat);
-            $stmt->bindValue(':longitude', $sLong);
-            $stmt->bindValue(':accessKey', $sId);
-            $stmt->execute();
-            sendResponse(200, 'saved new location', null);
+            $stmt = $db->prepare('SELECT user_id FROM account_verification WHERE access_key = :accessKey');
+            $stmt->bindValue(':accessKey', $accessToken);
+            if($stmt->execute()) {
+                $row = $stmt->fetchObject();
+                $user_id = $row->user_id;
+                $stmt = $db->prepare('UPDATE users SET latitude = :latitude, longitude = :longitude WHERE id = :id');
+                $stmt->bindValue(':latitude', $sLat);
+                $stmt->bindValue(':longitude', $sLong);
+                $stmt->bindValue(':id', $user_id);
+                $stmt->execute();
+                sendResponse(200, 'saved new location', null);
+            }
         } catch (PDOException $ex) {
             echo $ex;
         }
